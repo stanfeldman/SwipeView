121e3f388469f8e642437c1f154ce8c1528f91cf
Stan Feldman <stanislavfeldman@gmail.com>
2017-04-25 17:30:54 +0800

    created framework project to support Carthage

---
+++ SwipeView.h
@@ -0,0 +1,19 @@
+//
+//  SwipeView.h
+//  SwipeView
+//
+//  Created by Stan on 25/04/2017.
+//  Copyright © 2017 Limehat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwipeView.
+FOUNDATION_EXPORT double SwipeViewVersionNumber;
+
+//! Project version string for SwipeView.
+FOUNDATION_EXPORT const unsigned char SwipeViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipeView/PublicHeader.h>
+
+
